b952fa3bf8a16c860e45cd5866ac3e1e2e44010a
kouhei <syuzo7947@gmail.com>
2017-07-11 18:40:21 +0900

    done

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,5 @@ class Car < ApplicationRecord
   has_many :bookings
   #has_many :users, through: :bookings
   has_many :booked_users, through: :bookings, source: :user
-
   mount_uploader :photo, PhotoUploader
 end
